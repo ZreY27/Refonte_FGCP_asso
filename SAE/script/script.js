bf00207bd8c1481b2a1db7e2718a6008a6a9dd45
Lucioleee <purplelucie94@gmail.com>
2024-10-12 21:16:28 +0200

    Ancre

---
+++ SAE/script/script.js
@@ -0,0 +1,17 @@
+/*------------Ancre : apparaît apres avoir scroller ---------*/
+const backToTopButton = document.querySelector('.ancre');
+
+// Fonction pour afficher ou masquer la flèche en fonction du scroll
+window.onscroll = function() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        backToTopButton.style.display = "block"; // si on scroll plus de 200px la fleche apparait
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
+
+// Pour que ca défile fluidement
+backToTopButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
