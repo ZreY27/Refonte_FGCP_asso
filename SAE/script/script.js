e0b618186a673eefd0a32fe88f8abe558a111200
idkrm <147331552+idkrm@users.noreply.github.com>
2024-10-19 22:42:03 +0200

    version mobile ok sur les trois pages

--- SAE/script/script.js
+++ SAE/script/script.js
@@ -15,3 +15,22 @@ backToTopButton.addEventListener('click', function(e) {
     e.preventDefault();
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
+
+/*------------ Menu format mobile : apparaît et disparaît quand on clique sur l'image menu ---------*/
+const nav = document.querySelector('#navVersionMobile');
+const menuButton = document.getElementById('menuMobile');
+
+menuButton.addEventListener('click', function() {
+    if (nav.style.display === 'none' || nav.style.display === '') {
+        nav.style.display = 'block';
+        setTimeout(() => {
+            nav.style.maxHeight = '200px';
+        }, 0);
+    } else {
+        nav.style.maxHeight = '0'; // Ferme le menu
+        setTimeout(() => {
+            nav.style.display = 'none';
+        }, 500);
+    }
+});
