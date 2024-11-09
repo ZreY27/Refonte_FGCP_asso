a00d5f8d2d040e46f866281b1224bbde618c0ea5
antoine <a.reelitin@gmail.com>
2024-11-09 21:49:17 +0100

    caroussel

--- SAE/script/scriptActu.js
+++ SAE/script/scriptActu.js
@@ -155,49 +155,6 @@ filtresDiv.addEventListener('click', (event) => {
     btnClique(boutonActif);
 });
 
-
-/*------------ Menu format mobile : apparaît et disparaît quand on clique sur l'image menu ---------*/
-const nav = document.querySelector('#navVersionMobile');
-const menuButton = document.getElementById('menuMobile');
-
-menuButton.addEventListener('click', function() {
-    if (nav.style.display === 'none' || nav.style.display === '') {
-        nav.style.display = 'block';
-        setTimeout(() => {
-            nav.style.maxHeight = '200px';        }, 0);
-    }
-    else {
-        nav.style.maxHeight = '0'; // Ferme le menu
-        setTimeout(() => {
-            nav.style.display = 'none';
-        }, 500);
-    }
-});
-
-
-
-/*------------ Footer format PC petit écran : change comment est écrit l'adresse ---------*/
-// vérifie la taille de l'écran
-function verifierTailleEcran() {
-    const mediaQuery = window.matchMedia("(min-width: 768px) and (max-width: 992px)");
-    const adr = document.getElementById("adresse");
-
-    if (mediaQuery.matches) {
-        // Si l'écran est entre 768px et 992px, on change l'adresse
-        adr.innerHTML = "<img src=\"img/footer/contact/adresse.png\" alt=\"Adresse\" class=\"image\">" +
-            "Fédération France Greffes<br>Cœur et/ou Poumons<br>36 rue Petit, 75019 PARIS";
-    }
-    else{ // Sinon on garde la même
-        adr.innerHTML = "<img src=\"img/footer/contact/adresse.png\" alt=\"Adresse\" class=\"image\">" +
-            "Fédération France Greffes Cœur<br>et/ou Poumons<br>36 rue Petit, 75019 PARIS";
-    }
-}
-
-// Quand on modifie la taille de l'écran, ça appelle la fonction
-window.addEventListener('resize', verifierTailleEcran);
-
-
-
 // Charge les actus dans le caroussel des articles à la une
 let articlesTri = articles.sort((a, b) => {
     const dateA = new Date(a.date.split("/").reverse().join("-"));
